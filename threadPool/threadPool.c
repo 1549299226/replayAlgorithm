11a6377aa79615b973ab1a0ecb30f03a9ef27506
sunjichao <1549299226@qq.com>
2024-01-15 16:00:31 +0800

    [修改]补充了线程池结构体同时进一步完善了线程池初始化

--- threadPool/threadPool.c
+++ threadPool/threadPool.c
@@ -6,19 +6,48 @@
 
 #define DEFAULT_MIN_THREADS 5
 #define DEFAULT_MAX_THREADS 10
-
+#define DEFAULT_QUEUE_CAPACITY 100
 enum STATUS_CODE
 {
     ON_SUCCESS,
     NULL_PTR,
     MALLOC_ERROR,
     ACCESS_INVAILD,
-    UNKOWNN_ERROR
+    UNKNOWN_ERROR
 }
 
-void 
+//本质是一个消费者
+void * threadHander(void * arg)
+{
+    threadpool_t *pool = (threadpool_t *)arg;
+    while (1)
+    {
+       pthread_mutex_lock(&pool->mutexpool);
+        while (pool->queueSize == 0)
+        {
+            //等待一个条件变量，生产者发送过来的
+            pthread_cond_wait(&(pool->notEmpty), &(pool->mutexpool));
+        }
 
-int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
+        //任务队列有任务
+
+        task_t tmpTask = pool->taskQueue[pool->queueFront];
+        pool->queueFront = (pool->queueFront + 1) % pool->queueCapacity;
+        //任务数减一
+        pool->queueSize--;
+        //解锁
+        pthread_mutex_unlock(&(pool->mutexpool));
+        //发送一个信号给盛产这 告诉他可以继续生产
+        pthread_cond_signal(&(pool->notFull));
+        
+        //执行回调函数    
+        tmpTask.worker_hander(tmpTask.arg);   
+    }
+    pthread_exit(NULL);
+}
+
+//线程池初始化
+int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads, int queueCapacity)
 {
     if (pool == NULL)
     {
@@ -26,7 +55,7 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
     }
     do{
         //判断合法性
-        if (minThreads < 0 || maxThreads < 0 || minThreads >= maxThreads)
+        if (minThreads <= 0 || maxThreads <= 0 || minThreads >= maxThreads)
         {
             minThreads = DEFAULT_MIN_THREADS;
             maxThreads = DEFAULT_MAX_THREADS;
@@ -37,6 +66,24 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
         pool->minThreads = minThreads;
         pool->maxThreads = maxThreads;
 
+        //判断合法性
+        if (queueCapacity <= 0)
+        {
+            queueCapacity = DEFAULT_QUEUE_CAPACITY;
+        }
+        
+        //更新线程池 任务队列属性
+        pool->queueCapacity = queueCapacity;
+        pool->queueFront = 0;
+        pool->queueRear = 0;
+        pool->queueSize = 0;
+        pool->taskQueue = (task_t *)malloc(sizeof(task_t) * pool->taskQueue);
+        if (pool->taskQueue == NULL)
+        {
+            perror("malloc error");
+            break;
+        }
+        
         //为线程ID分配堆空间
         pool->threadIds = (pthread_t *)malloc(sizeof(pthread_t) * maxThreads);
         if (pool->threadIds == NULL)
@@ -54,7 +101,7 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
             //如果线程id号为零这个位置可用
             if (pool->threadIds[idx] == 0)
             {
-                ret = pthread_create((&pool->threadIds[idx]), NULL, threadHander, NULL);
+                ret = pthread_create((&pool->threadIds[idx]), NULL, threadHander, pool);
                 if (ret != 0)
                 {
                     perror("thread create error");
@@ -62,9 +109,25 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
                 }
             }
         }
+        //此ret是创建线程函数的返回值
+        if (ret != 0)
+        {
+            break;
+        }
+        //初始化所资源
+        pthread_mutex_init(&(pool->mutexpool), NULL);
+        pthread_cond_init(&(pool->notEmpty), NULL);
+        pthread_cond_init(&(pool-notFull), NULL);
+
         return ON_SUCCESS;
-    }while(0);
+    } while(0);
     //程序执行到这里，上面一定有失败
+    
+    if (pool->taskQueue)
+    {
+        free(pool->taskQueue);
+        pool->taskQueue = NULL;
+    }
     
     //回收线程资源
     for (int idx = 0; idx < pool->minThreads; idx++)
@@ -72,7 +135,7 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
         if (pool->threadIds[idx] != 0)
         {
             
-            pthread_join(pool->threadIds[idx]);
+            pthread_join(pool->threadIds[idx], NULL);
         }
         
     }
@@ -81,7 +144,7 @@ int threadPoolInit(threadpool_t *pool, min minThreads, int maxThreads)
         free(pool->threadIds);
         pool->threadIds = NULL;
     }
-    return UNKOWNN_ERROR
+    return UNKNOWN_ERROR;
 }   
 
 
--- threadPool/threadpool.h
+++ threadPool/threadpool.h
@@ -1,16 +1,45 @@
 #ifndef __THREAD_POOL_H_
 #define __THREAD_POOL_H_
+
 #include <pthread.h>
 
+typedef struct task_t
+{
+    //回调函数
+    void *(*worker_hander)(void *arg);
+    //参数
+    void *arg;
+
+} task_t;
 //线程池结构体
 typedef struct threadpool_t
 {
+    //任务队列
+    task_t * taskQueue;
+    //任务队列的容量
+    int queueCapacity;
+    //任务队列的任务数
+    int queueSize;
+    //任务队列的队头
+    int queueFront;
+    //任务队列的队尾
+    int queueRear;
+
     //线程池中的线程
     pthread_t *threadIds;
     //最小的线程数
     int minThreads;
     //最大的线程数
     int maxThreads;
+
+    //锁
+    pthread_mutex_t mutexpool;
+
+    //条件变量:任务队列有任务可消费
+    pthread_cond_t notEmpty;
+
+    //条件变量:任务队列有空位 可以继续放
+    pthread_cond_t notFull;
     
 }threadpool_t;
 
